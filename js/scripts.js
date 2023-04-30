713fb07fec644c5f4d2b1f6127d50a47ee9bba5c
Arafat Ibne Ikram <arafatikram47@gmail.com>
2023-04-30 17:24:00 +0600

    new

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,21 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+jQuery(function ($) {
 
-window.addEventListener('DOMContentLoaded', event => {
+    'use strict';
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+    // --------------------------------------------------------------------
+    // PreLoader
+    // --------------------------------------------------------------------
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
 
-});
+
+
+    // --------------------------------------------------------------------
+    // Sticky Sidebar
+    // --------------------------------------------------------------------
+
+    $('.left-col-block, .right-col-block').theiaStickySidebar();
+
+}); // JQuery end
